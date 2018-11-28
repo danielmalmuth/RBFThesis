67ba98dd3fab936e8a9510bd3f37024b567130d9
Daniel Malmuth <danielmalmuth@macbook-air-4.boisestate.edu>
2018-11-28 14:46:39 -0700

    Revised code for testing various configurations

---
+++ rk4_sla.m
@@ -0,0 +1,50 @@
+function q_new = ...
+    rk4_sla(delta_t,u_func,x,int_leng,q_init,target_time, ...
+    rbf_exp,deg)
+
+q_new = q_init;
+q_new_ext = [q_new;q_new;q_new];
+
+x_ext = [x-int_leng;x;x+int_leng];
+tree = KDTreeSearcher(x_ext);
+
+for i = 1:length(x)
+    idx = knnsearch(tree,x(i),'k',7);
+    S{i}.neighbors = idx;
+    S{i}.d = localinterpmat_1D(x_ext(idx),rbf_exp,deg);
+end
+% map = repmat((1:length(x))',[3 1]);
+
+for i = 1:round((target_time/delta_t))
+    k1 = -delta_t.*u_func(i*delta_t,x);
+    k2 = -delta_t.*u_func(i*delta_t + -(delta_t/2),x + (k1/2));
+    k3 = -delta_t.*u_func(i*delta_t + -(delta_t/2),x + (k2/2));
+    k4 = -delta_t.*u_func(i*delta_t + -(delta_t),x + k3);
+    x_new = x + (k1 + 2*k2 + 2*k3 + k4)./6;
+ 
+    for j = 1:length(x)
+        idx = knnsearch(tree,x_new(j),'k',1);
+        idx = mod(idx-1,length(x))+1;
+        lam = S{idx}.d\[q_new_ext(S{idx}.neighbors);zeros(deg+1,1)];
+        
+%         lam = rbffit_test_1D(x_ext(idx),q_new_ext(idx),"Backslash",true,0,2); 
+        if x_new(j) > (x_ext(2*length(x)) + x_ext(2*length(x)+1))/2
+            x_new(j) = x_new(j) - x_ext(2*length(x)+1);
+        elseif x_new(j) < (x_ext(length(x)+1) + x_ext(length(x)))/2
+            x_new(j) = x_ext(2*length(x)+1) + x_new(j);
+        end
+        q_new(j) = ...
+            localrbfinterp_1D(lam,x_new(j),x_ext(S{idx}.neighbors),...
+            rbf_exp,deg); % Local method
+        
+%         q_new = rbfval2_1D(lam,x(idx),x_new,1); % Global method
+
+    end
+    q_new_ext = [q_new;q_new;q_new];
+
+%     plot(x,q_new,'k-')
+%     ylim([0 3])
+%     pause(0.005);
+end
+
+end
--- sfdm.m
+++ sfdm.m
@@ -1,5 +1,7 @@
-clc
+% clc
 close all
+
+warning('off','MATLAB:nearlySingularMatrix')
 
 % Functions
 % q = @(x) exp(-(60.*(x-0.1)).^2); % initial distribution
@@ -13,58 +15,72 @@ b = 1;
 
 u = @(t,x) -sin(2.*pi.*t).*(1+sin(2.*pi.*x)); % u in advection equation
 target_time = 1;
-numtimes = 4; % number of h to test
-error = zeros(numtimes,1);
+numtimes = 4; % number of delta_t to test
 max_u = 2;%u(fminbnd(@(t,x) -u(t,x),0,target_time)); % Max value of u function
-cfl = 4; % CFL number
+cfl = 3; % CFL number
+
+powers = [3 5 7 9];
+degrees = [4 4 4 4];
+numtests = length(powers);
+
+error = zeros(numtimes,numtests);
 
 delta_t = 1/16*(1./2.^(1:numtimes))'; % vector of delta_t values
 
 h = delta_t*max_u/cfl; % hs based on delta_t values
+for i = 1:numtests
+    for j = 1:numtimes
+        n = floor((b-a)/h(j));
+        x = linspace(a,b,n+1); x = x(1:n)';
+        q_vec = q(x);
 
-for j = 1:numtimes
-    n = (b-a)/h(j);
-    x = linspace(a,b,n+1); x = x(1:n)';
-    q_vec = q(x);
+    % % Centered Difference 2nd order with RK4
+    %     pos_center = (1/(2*h(j)))*ones((n),1);
+    %     neg_center = (-1/(2*h(j)))*ones((n),1);
+    %     D = spdiags([pos_center neg_center],[1 -1],n,n);
+    %     D(n,1) = (1/(2*h(j)));
+    %     D(1,n) = -(1/(2*h(j)));
+    %     q_approx = rk4_adv(delta_t(j),u,D,q_vec,target_time);
 
-% % Centered Difference 2nd order with RK4
-%     pos_center = (1/(2*h(j)))*ones((n),1);
-%     neg_center = (-1/(2*h(j)))*ones((n),1);
-%     D = spdiags([pos_center neg_center],[1 -1],n,n);
-%     D(n,1) = (1/(2*h(j)));
-%     D(1,n) = -(1/(2*h(j)));
-%     q_approx = rk4_adv(delta_t(j),u,D,q_vec,target_time);
-    
-% % Centered Difference 4th order with RK4
-%     pos2_center = (-1/(12*h(j)))*ones((n),1);
-%     pos1_center = (8/(12*h(j)))*ones((n),1);
-%     neg1_center = (-8/(12*h(j)))*ones((n),1);
-%     neg2_center = (1/(12*h(j)))*ones((n),1);
-%     D = spdiags([pos2_center pos1_center neg1_center neg2_center ...
-%         pos2_center neg2_center],[2 1 -1 -2 -(n-2) (n-2)],n,n);
-%     D(n,1) = (8/(12*h(j)));
-%     D(1,n) = (-8/(12*h(j)));
-%     q_approx = rk4_adv(delta_t(j),u,D,q_vec,target_time);
+    % % Centered Difference 4th order with RK4
+    %     pos2_center = (-1/(12*h(j)))*ones((n),1);
+    %     pos1_center = (8/(12*h(j)))*ones((n),1);
+    %     neg1_center = (-8/(12*h(j)))*ones((n),1);
+    %     neg2_center = (1/(12*h(j)))*ones((n),1);
+    %     D = spdiags([pos2_center pos1_center neg1_center neg2_center ...
+    %         pos2_center neg2_center],[2 1 -1 -2 -(n-2) (n-2)],n,n);
+    %     D(n,1) = (8/(12*h(j)));
+    %     D(1,n) = (-8/(12*h(j)));
+    %     q_approx = rk4_adv(delta_t(j),u,D,q_vec,target_time);
 
-% % Upwinding Scheme
-%     q_approx = fe_adv_up(delta_t(j),u,q_vec,target_time,h(j));
+    % % Upwinding Scheme
+    %     q_approx = fe_adv_up(delta_t(j),u,q_vec,target_time,h(j));
 
-% Semi-Legrangian Advection
-    q_approx = rk4_adv_sla(delta_t(j),u,x,q_vec,target_time,2);
+    % Semi-Legrangian Advection
+        q_approx = rk4_sla(delta_t(j),u,x,(b-a),q_vec,target_time,...
+            powers(i),degrees(i));
 
-    
-    
-%     q_true = q_sol(u,x,target_time)';
-%     error(j) = max(abs(q_true - q_approx))/max(abs(q_true));
-    error(j) = max(abs(q_vec - q_approx))/max(abs(q_vec));
+
+
+    %     q_true = q_sol(u,x,target_time)';
+    %     error(j) = max(abs(q_true - q_approx))/max(abs(q_true));
+        error(j,i) = max(abs(q_vec - q_approx))/max(abs(q_vec));
+    end
 end
-
 %%
-
-loglog(h,error,'x-')
-xlabel('h')
+for i = 1:numtests
+    loglog(h,error(:,i),'x-')
+    hold on
+end
+xlabel('$h$','Interpreter','Latex')
 ylabel('Relative $L_2$ Error','Interpreter','Latex')
-title('Relative $L_2$ Error','Interpreter','Latex')
+title('Relative $L_2$ Error, Degree = 4','Interpreter','Latex')
+legend('r^3 - O(5.0083)','r^5 - O(4.0957)','r^7 - O(4.2178)',...
+    'r^9 - O(5.7320)')
 
-x = polyfit(log(h(2:end)),log(error(2:end)),1);
-order = x(1);
+orders = zeros(numtests,1);
+
+for i = 1:numtests
+    x = polyfit(log(h(2:end)),log(error(2:end,i)),1);
+    orders(i) = x(1);
+end
